1a2d4e0186b98494658562b503b49c6e55222948
wesley barros pereira <barroswesley902@gmail.com>
2025-07-09 21:41:01 +0000

    desafio 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Main } from "./components/Main";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { LuckyNumber } from "./components/LuckyNumber";
+// import { Main } from "./components/Main";
+// import { Header } from "./components/Header";
+// import { Footer } from "./components/Footer";
+// import { LuckyNumber } from "./components/LuckyNumber";
+import { ProductList } from "./components/productlist";
 
 export default function App() {
 
   return (
     
     <>
-      <Header />
+      {/* <Header /> */}
       {/* <Main /> */}
       {/* <Footer /> */}
-      <LuckyNumber />
+      {/* <LuckyNumber /> */}
+      <ProductList/>
     </>
   );
 }
